a27effd501f285200400259a114dc41f7af0a6a8
summerCat <583188551@qq.com>
2019-01-14 11:14:04 +0800

    提交 对于hystrix 监控的两种方式 Dashboard 和turbine

---
+++ hystrixDashboard/src/main/java/com/linjing/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.linjing.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+//@SpringBootApplication
+@EnableHystrixDashboard
+@SpringCloudApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
+
